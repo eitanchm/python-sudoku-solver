8f8bf15b634c4a03d991c001edb8374b3c3cc1c9
eitanchm <eitan4562@gmail.com>
2020-03-11 13:49:20 +0200

    set up for implementing GUI

---
+++ Sudoku Solver/GUI.py
@@ -0,0 +1,12 @@
+######################################
+# GUI Class for Python Sudoku solver #
+#      Credit: Eitan Chmelevsky      #
+######################################
+
+import pygame
+
+
+class Grid:
+
+	def __init__(self, rows, cols, width, height, win):
+		
