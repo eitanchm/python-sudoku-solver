b994ffe7a7c195245018ad86b18bdf014db77173
eitanchm <eitan4562@gmail.com>
2020-03-11 14:10:07 +0200

    added Grid class

--- Sudoku Solver/GUI.py
+++ Sudoku Solver/GUI.py
@@ -8,5 +8,14 @@ import pygame
 
 class Grid:
 
-	def __init__(self, rows, cols, width, height, win):
+	def __init__(self, rows, cols, width, height):
+		self.rows = rows
+		self.cols = cols
+		self.width = width
+		self.height = height
+		pygame.init()
+
+
+	def draw(self):
+		pygame.display.set_mode((self.width, self.height))
 		
